f7b2fb56a32d0e18a031dcb032b00a45cfcda50a
aravindravula <84632446+aravindravula@users.noreply.github.com>
2021-06-11 15:19:51 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,16 @@
+a=10.015
+print(type(a),a)
+b=int(a)
+print(type(b),b)
+c=str(b)
+print(type(c),c)
+d=bool(c)
+print(type(d),d)
+
+Name="ARAVIND RAVULA"
+Weight=60
+Height=5.5
+Vegeterian=True
+P="my Name is {},my Weight is {}kg ,my Height is {} feet and iam a Vegeterian{}".format(Name,Weight,Height,Vegeterian)
+print(P)
+      
