64d586313cee8bdcde8335eca99f755c65f2755e
aravindravula <84632446+aravindravula@users.noreply.github.com>
2021-06-13 15:29:06 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,27 @@
+ #age calculator
+Birthyear=int(input("enter birthyear:"))
+Currentyear=int(input("enter current year:"))
+Age=Currentyear-Birthyear
+print(Age)
+
+ #calculator program
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+ #addition
+print('{}+{}'.format(num1,num2))
+print(num1+num2)
+ #substraction
+print('{}-{}'.format(num1,num2))
+print(num1-num2)
+ #multiplication
+print('{}*{}'.format(num1,num2))
+print(num1*num2)
+ #division
+print('{}/{}'.format(num1,num2))
+print(num1/num2)
+ #floor division
+print('{}//{}'.format(num1,num2))
+print(num1//num2)
+ #exponent
+print('{}**{}'.format(num1,num2))
+print(num1**num2)
